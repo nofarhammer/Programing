b170bd5d623c2c3d16f7544a617e32b8a5bd7fd1
nofarhammer <nofarhammer@gmail.com>
2015-08-06 11:55:51 +0800

    Update rotationColor.py

--- rotationColor.py
+++ rotationColor.py
@@ -11,16 +11,29 @@ rot=mraa.Aio(0)
 x=mraa.I2c(0)
 x.address(0x62)
 
+# Set up the PWM
+pwm = mraa.Pwm(5)
+pwm.enable(True)
+pwm.period_us(5000)
+
+# Set up the ADC
+adc = mraa.Aio(0)
+
 # initialise device
 x.writeReg(0, 0)
 x.writeReg(1, 0)
 
 # sent RGB color data
 while c<10:
-        print (rot.read())
+       # print (rot.read())
         r=randrange(0,256)
         g-randrange(0,256)
         b=randrange(0,256)
         x.writeReg(0x08, r)
         x.writeReg(0x04, g)
         c=c+1
+        value = adc.read()             # Read the ADC value
+        print (adc)
+        led_intensity = value/ROT_MAX  # Determine the duty cycle based on ADC value
+        pwm.write(led_intensity)   
+        time.sleep(0.5)
