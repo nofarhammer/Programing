b4048e3a7e1189d29ccf40aaee7bfbec7f0eeffe
nofarhammer <nofarhammer@gmail.com>
2015-08-07 11:42:11 +0800

    Update rotationColor.py

--- rotationColor.py
+++ rotationColor.py
@@ -9,15 +9,15 @@ g=0
 b=0
 
 # Set up the LCD
-x=mraa.I2c(0)
-x.address(0x62)
+LCD=mraa.I2c(0)
+LCD.address(0x62)
 
 # Set up the ADC
 adc = mraa.Aio(0)
 
 # initialise device
-x.writeReg(0, 0)
-x.writeReg(1, 0)
+LCD.writeReg(0, 0)
+LCD.writeReg(1, 0)
 
 # sent RGB color data
 while 1:
@@ -25,8 +25,7 @@ while 1:
         r=(math.floor(degree)*10+400)%256
         g=(math.floor(degree)*15+150)%256
         b=(math.floor(degree)*20+1)%256
-        #x.writeReg(0x1c, r)
-        #x.writeReg(0x08, g)
-        #x.writeReg(0x02, b)
-        print r, g, b
+        LCD.writeReg(0x08, r)
+        LCD.writeReg(0x04, g)
+        LCD.writeReg(0x02, b)
         time.sleep(0.5)
