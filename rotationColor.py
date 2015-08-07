6a7d64740ec7881356393272716a47e81122b1fa
nofarhammer <nofarhammer@gmail.com>
2015-08-07 11:27:05 +0800

    Update rotationColor.py

--- rotationColor.py
+++ rotationColor.py
@@ -8,14 +8,9 @@ r=0
 g=0
 b=0
 
-rot=mraa.Aio(0)
+# Set up the LCD
 x=mraa.I2c(0)
 x.address(0x62)
-
-# Set up the PWM
-pwm = mraa.Pwm(5)
-pwm.enable(True)
-pwm.period_us(5000)
 
 # Set up the ADC
 adc = mraa.Aio(0)
@@ -25,13 +20,12 @@ x.writeReg(0, 0)
 x.writeReg(1, 0)
 
 # sent RGB color data
-while c<10:
+while 1:
         degree = adc.read()             # Read the ADC value
-       # r=(math.floor(degree)*10+400)%100
-        g=(math.floor(degree)*15+150)%100
-        b=(math.floor(degree)*20+1)%100
-        x.writeReg(0x08, 0xAA)
+        r=(math.floor(degree)*10+400)%256
+        g=(math.floor(degree)*15+150)%256
+        b=(math.floor(degree)*20+1)%256
+        x.writeReg(0x08, r)
         x.writeReg(0x04, g)
         x.writeReg(0x02, b)
-        c=c+1
         time.sleep(0.5)
